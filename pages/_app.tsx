876e325780ea27f1acbe71fafbcd076890e59d30
Tin Trung Nguyen <hopthucuatin@gmail.com>
2021-11-06 17:29:25 +0700

    Fix build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,10 @@
-import { ChakraProvider, CSSReset } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import Layout from "../common/components/Layout";
-import {
-  default as theme,
-  cssResetConfig as config,
-} from "../common/config/charkaTheme";
+import { default as theme } from "../common/config/charkaTheme";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
-      <CSSReset config={config} />
       <Layout>
         <Component {...pageProps} />
       </Layout>
